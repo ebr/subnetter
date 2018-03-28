cbc11ccd53db9217035614e86ff53005c6a60a72
Eugene Brodsky <ebrodsky@kl-ebrodsky>
2018-03-28 10:05:38 -0400

    --wip-- [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,22 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const filter = require('gulp-filter');
 const uglify = require('gulp-uglify');
+const mainYarnFiles = require('main-yarn-files');
 
-const mainBowerFiles = require('main-bower-files');
-
-    gulp.task('bower', function(cb) {
-        var files = mainBowerFiles(),
-            jsFilter = filter('**/*.js', {restore: true}),
+    gulp.task('default', function(cb) {
+        var files = mainYarnFiles();
+        var jsFilter = filter('**/*.js', {restore: true}),
             cssFilter = filter('**/*.css', {restore: true}),
             fontFilter = filter('**/*.{eot,svg,ttf,woff,woff2}', {restore: true}),
 
             everythingElseFilter = filter([ '**/*.!{js,css}' ], {restore: true})
             //onError = createOnError(cb);
-
+        
         if (!files.length) {
             return cb();
         }
 
-        gulp.src(mainBowerFiles())
+        gulp.src(mainYarnFiles())
          .pipe(jsFilter)
          .pipe(concat('vendor.js'))
          .pipe(uglify())
